40c1a2e89f139a2dc0268bf94d431dd3320f5f80
vivek8271 <vivekkumar37904@gmail.com>
2025-12-18 16:17:23 +0530

    Update quick-quote-form.js

--- api/quick-quote-form.js
+++ api/quick-quote-form.js
@@ -1,19 +1,23 @@
-import { connectDB } from "../lib/db";
-import QuickQuoteForm from "../models/QuickQuoteForm";
+// import { connectDB } from "../lib/db";
+// import QuickQuoteForm from "../models/QuickQuoteForm";
 
-export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ message: "Only POST allowed" });
-  }
+// export default async function handler(req, res) {
+//   if (req.method !== "POST") {
+//     return res.status(405).json({ message: "Only POST allowed" });
+//   }
 
-  try {
-    await connectDB();
-    const newEntry = new QuickQuoteForm(req.body);
-    await newEntry.save();
+//   try {
+//     await connectDB();
+//     const newEntry = new QuickQuoteForm(req.body);
+//     await newEntry.save();
 
-    res.status(200).json({ success: true, message: "✅ Form saved to MongoDB" });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ success: false, message: "❌ Failed to save form" });
-  }
+//     res.status(200).json({ success: true, message: "✅ Form saved to MongoDB" });
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).json({ success: false, message: "❌ Failed to save form" });
+//   }
+// }
+
+export default function handler(req, res) {
+  res.status(200).json({ ok: true });
 }
