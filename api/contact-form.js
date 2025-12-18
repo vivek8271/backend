d69bf2913542a2231dd3b8acaf335bd13014ef14
vivek8271 <vivekkumar37904@gmail.com>
2025-12-18 16:32:07 +0530

    Fix import path for ContactForm model

--- api/contact-form.js
+++ api/contact-form.js
@@ -1,5 +1,5 @@
 import { connectDB } from "../lib/db.js";
-import ContactForm from "../models/ContactForm.js";
+import ContactForm from "../model/ContactForm.js";
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
@@ -17,4 +17,5 @@ export default async function handler(req, res) {
     res.status(500).json({ success: false, message: "❌ Failed to save form" });
   }
 }
+
 
