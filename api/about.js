9e9ca039641e5d4892a99e69d20843ecb3768d37
vivek8271 <vivekkumar37904@gmail.com>
2025-12-26 10:23:55 +0530

    Update about.js

--- api/about.js
+++ api/about.js
@@ -1,6 +1,10 @@
+import fs from "fs";
+import path from "path";
 
-import about from "../templates/about.html"
+export default function handler(req, res) {
+  const htmlPath = path.join(process.cwd(), "templates", "about.html");
+  const html = fs.readFileSync(htmlPath, "utf-8");
 
-export function aboutPage(req, res){
-  req.render(about);
+  res.setHeader("Content-Type", "text/html");
+  res.status(200).send(html);
 }
