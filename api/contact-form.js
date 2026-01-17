e9f4f86034f489d414b5d96bc4b191b43d3e71f4
vivek8271 <vivekkumar37904@gmail.com>
2026-01-18 01:16:49 +0530

    Refactor contact form handler for clarity

--- api/contact-form.js
+++ api/contact-form.js
@@ -1,27 +1,3 @@
-// import { connectDB } from "../lib/db.js";
-// import ContactForm from "../model/ContactForm.js";
-
-// export default async function handler(req, res) {
-//   if (req.method !== "POST") {
-//     return res.status(405).json({ message: "Only POST allowed" });
-//   }
-
-//   try {
-//     await connectDB();
-//     const newEntry = new ContactForm(req.body);
-//     await newEntry.save();
-
-//     res.status(200).json({ success: true, message: "✅ Form saved to MongoDB" });
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ success: false, message: "❌ Failed to save form" });
-//   }
-// }
-
-
-
-
-
 import { connectDB } from "../lib/db.js";
 import ContactForm from "../model/ContactForm.js";
 
@@ -59,4 +35,5 @@ export default async function handler(req, res) {
     });
   }
 }
+
 
